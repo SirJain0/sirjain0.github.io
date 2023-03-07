04948d5dc548207eb898c34b9eaac30d4af7d176
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-03-06 20:07:29 -0500

    Clean up a bit of formatting

--- src/minecraft_projects.js
+++ src/minecraft_projects.js
@@ -21,30 +21,32 @@ const projects = [
 
 const projectList = document.getElementById("project-list") //get the plugin list element
 
+// Iterate and display each project
 for (const project of projects) {
     const div = document.createElement("bb-project");
     div.className = "bb-project";
     div.classList.add("project-view");
+
     div.innerHTML = 
     `
-    <div class="project-header">
-        <div class="box">
-            <h2 class="project-title">${project.name}</h2>
+        <div class="project-header">
+            <div class="box">
+                <h2 class="project-title">${project.name}</h2>
+            </div>
+
+            <div class="box2">
+                <h3 class="project-version">${project.version}</h3>
+            </div>
         </div>
 
-        <div class="box2">
-            <h3 class="project-version">${project.version}</h3>
+        <div>
+            <p class="project-description">${project.description}</p>
         </div>
-    </div>
 
-    <div>
-        <p class="project-description">${project.description}</p>
-    </div>
-
-    <div class="links">
-        <a class="project-link" href="${project.link}">Official Link</a>
-    </div>
+        <div class="links">
+            <a class="project-link" href="${project.link}">Official Link</a>
+        </div>
     `;
+
     projectList.append(div)
 }
-console.log(projects)
