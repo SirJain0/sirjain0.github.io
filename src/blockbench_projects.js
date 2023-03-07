c7bcb6f37c9cc32110db50ad2008daec368b0059
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-03-06 20:09:23 -0500

    Formatting pt 2

--- src/blockbench_projects.js
+++ src/blockbench_projects.js
@@ -3,9 +3,12 @@ const data = await request.json()
 const plugins = Object.entries(data).filter(e => e[1].author.includes("SirJain"))
 
 const pluginList = document.getElementById("project-list") //get the plugin list element
+
+// Iterate and display each project
 for (const [id, plugin] of plugins) {
     let pluginID = plugin.title.toLowerCase().replaceAll(" ", "_");
 
+    // Skip over MC Text Generator - has special handling
     if (plugin.title === "Minecraft Text Generator") continue;
 
     const div = document.createElement("bb-project");
@@ -13,25 +16,24 @@ for (const [id, plugin] of plugins) {
     div.classList.add("project-view");
     div.innerHTML = 
     `
-    <div class="project-header">
-        <div class="box">
-            <h2 class="project-title">${plugin.title}</h2>
+        <div class="project-header">
+            <div class="box">
+                <h2 class="project-title">${plugin.title}</h2>
+            </div>
+
+            <div class="box2">
+                <h3 class="project-version">${plugin.version}</h3>
+            </div>
         </div>
 
-        <div class="box2">
-            <h3 class="project-version">${plugin.version}</h3>
+        <div>
+            <p class="project-description">${plugin.description}</p>
         </div>
-    </div>
 
-    <div>
-        <p class="project-description">${plugin.description}</p>
-    </div>
-
-    <div class="links">
-        <a class="project-link" href="http://www.blockbench.net/plugins/${pluginID}">Official Link</a>
-    </div>
+        <div class="links">
+            <a class="project-link" href="http://www.blockbench.net/plugins/${pluginID}">Official Link</a>
+        </div>
     `;
+
     pluginList.append(div)
 }
-
-console.log(plugins)
