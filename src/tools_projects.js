9490ccaf5749c2252a5271e37d77245627b4faab
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-04-01 20:05:14 -0400

    Initial implementation

---
+++ src/tools_projects.js
@@ -0,0 +1,45 @@
+const projects = [
+    {
+        "name": "Cite Generator",
+        "description": "A small tool that allows you to generate a cite for a website, which can be used for bibliographies!",
+        "version": "Unreleased",
+        "author": "SirJain",
+        "link": "n/a" 
+    }
+]
+
+const projectList = document.getElementById("project-list") //get the plugin list element
+
+// Iterate and display each project
+for (const project of projects) {
+    const div = document.createElement("bb-project");
+    div.className = "bb-project";
+    div.classList.add("project-view");
+
+    div.innerHTML = 
+    `
+        <div class="project-header">
+            <div class="box">
+                <h2 class="project-title">${project.name}</h2>
+            </div>
+
+            <div class="box2">
+                <h3 class="project-version">${project.version}</h3>
+            </div>
+        </div>
+
+        <div>
+            <p class="project-authors">By: ${project.author}</p>
+        </div>
+
+        <div>
+            <p class="project-description">${project.description}</p>
+        </div>
+
+        <div class="links">
+            <a class="project-link" href="${project.link}">Official Link</a>
+        </div>
+    `;
+
+    projectList.append(div)
+}
