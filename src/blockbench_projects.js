cb4ab4f03d0bc6e0c7116e2ce96efa40a28030a9
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-04-09 13:17:20 -0400

    Generate some HTML in code in Blockbench projects list

--- src/blockbench_projects.js
+++ src/blockbench_projects.js
@@ -1,12 +1,62 @@
-const request = await fetch("https://raw.githubusercontent.com/JannisX11/blockbench-plugins/master/plugins.json")
-const data = await request.json()
-const plugins = Object.entries(data).filter(e => e[1].author.includes("SirJain"))
+const request = await fetch("https://raw.githubusercontent.com/JannisX11/blockbench-plugins/master/plugins.json");
+const data = await request.json();
+const plugins = Object.entries(data).filter(e => e[1].author.includes("SirJain"));
+
+const projects = [
+    {
+        "name": "Blockmorphism",
+        "description": "A theme that effectively brings the Glassmorphism style into Blockbench!",
+        "version": "1.2.0",
+        "author": "SirJain",
+        "link": "https://github.com/SirJain0/Blockmorphism" 
+    },
+    {
+        "name": "Minecraft Text Generator",
+        "description": "Generates Minecraft-styled text in cubes.",
+        "version": "2.0.0",
+        "author": "SirJain",
+        "link": "https://blockbench.net/plugins/mc_text_generator" 
+    }
+]
 
 // Get the plugin list element
-const pluginList = document.getElementById("project-list") 
+const projectList = document.getElementById("project-list");
+
+for (const project of projects) {
+    const div = document.createElement("bb-project");
+    div.className = "bb-project";
+    div.classList.add("project-view");
+
+    div.innerHTML = 
+    `
+        <div class="project-header">
+            <div class="box">
+                <h2 class="project-title">${project.name}</h2>
+            </div>
+
+            <div class="box2">
+                <h3 class="project-version">${project.version}</h3>
+            </div>
+        </div>
+
+        <div>
+            <p class="project-authors">By: ${project.author}</p>
+        </div>
+
+        <div>
+            <p class="project-description">${project.description}</p>
+        </div>
+
+        <div class="links">
+            <a class="project-link" href="${project.link}">Official Link</a>
+        </div>
+    `;
+
+    projectList.append(div)
+}
 
 // Iterate and display each project
-for (const plugin of plugins) {
+for (const [id, plugin] of plugins) {
     let pluginID = plugin.title.toLowerCase().replaceAll(" ", "_");
 
     // Skip over MC Text Generator and Outline Creator
@@ -40,5 +90,5 @@ for (const plugin of plugins) {
         </div>
     `;
 
-    pluginList.append(div)
+    projectList.append(div)
 }
