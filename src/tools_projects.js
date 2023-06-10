5259dcd84e422ee70f8e1bf21d8faa2829f32294
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-06-10 13:21:57 -0400

    Fix a couple UI issues

--- src/tools_projects.js
+++ src/tools_projects.js
@@ -22,6 +22,7 @@ for (const project of projects) {
     `
         <div class="project-header">
             <h3 class="project-title">${project.name}</h2>
+            <div class="spacer"></div>
             <a class="project-link" href="${project.link}">
                 <span class="material-symbols-outlined">open_in_new</span>
             </a>
