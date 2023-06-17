c627eede57658fbc8395581c691b91620485a48d
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-06-17 11:51:25 -0400

    Bump Extensive Diamonds version number

--- src/minecraft_projects.js
+++ src/minecraft_projects.js
@@ -2,7 +2,7 @@ const projects = [
     {
         "name": "Extensive Diamonds",
         "description": "Mod that adds three new diamond types to Minecraft!",
-        "version": "1.1.0",
+        "version": "1.1.1",
         "author": "SirJain",
         "link": "https://www.curseforge.com/minecraft/mc-mods/extensive-diamonds/",
         "translation": "https://poeditor.com/join/project/rUCEF9wpEF",
