2044d8aeec7606b467b96ed3449219a08c700edb
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-08-28 10:31:52 -0400

    Get rid of index.html at end of URL
    Also, rename misc URL path to misc_projects

--- src/main_pg_projects.js
+++ src/main_pg_projects.js
@@ -2,17 +2,17 @@ const views = [
     {
         "title": "Blockbench Projects",
         "description": "Includes plugins that extend the Blockbench functionality and themes that touch up the appearance of the program.",
-        "path": "blockbench_projects/index.html"
+        "path": "blockbench_projects/"
     },
     {
         "title": "Minecraft Projects",
         "description": "Projects that tie in programming and art skills shown in a wide range of survival-oriented and utility mods.",
-        "path": "minecraft_projects/index.html"
+        "path": "minecraft_projects/"
     },
     {
         "title": "Miscellaneous",
         "description": "Miscellaneous projects which don't fall under any category, most commonly tools.",
-        "path": "misc/index.html"
+        "path": "misc_projects/"
     }
 ]
 
