6eedbdd6b30d0051dbfc5efdf043dd1bd7ce3925
SirJain <94301223+SirJain0@users.noreply.github.com>
2023-08-29 12:23:31 -0400

    Update blockbench_projects.js

--- src/blockbench_projects.js
+++ src/blockbench_projects.js
@@ -82,52 +82,40 @@ for (const project of projects) {
 }
 
 // Iterate and display each project
-for (const [id, project] of projects) {
+for (const [id, plugin] of plugins) {
+    let pluginID = plugin.title.toLowerCase().replaceAll(" ", "_");
+
+    // Skip over Text Generator plugin
+    if (plugin.title === "Text Generator") continue;
+
     const div = document.createElement("bb-project");
     div.className = "bb-project";
     div.classList.add("project-view");
 
-    let innerHTML = `
+    div.innerHTML = 
+    `
         <div class="project-header">
-        <h3 class="project-title">${project.name}</h2>
-        <div class="spacer"></div>
-    `
-
-    if (project.translation) innerHTML += `
-        <a class="project-link" style="margin-right: -5px" href="${project.translation}" target="_blank">
-            <span class="material-symbols-outlined" style="font-size: 20px;">language</span>
-        </a>
-    `
-
-    innerHTML += `
-        <a class="project-link" href="${project.link}" target="_blank">
-            <span class="material-symbols-outlined" style="font-size: 20px;">open_in_new</span>
-        </a>
+            <h3 class="project-title">${plugin.title}</h2>
+            <div class="spacer"></div>
+            <a class="project-link" href="https://blockbench.net/plugins/${pluginID}" target="_blank">
+                <span class="material-symbols-outlined">open_in_new</span>
+            </a>
         </div>
-    `
-
-    if (project.author) innerHTML += `
-        <p class="project-authors">With ${project.author}</p>
-    `
-
-    innerHTML += `
-        <p class="project-description">${project.description}</p>
+        <p class="project-authors">By: ${plugin.author}</p>
+        <p class="project-description">${plugin.description}</p>
         <div class="spacer"></div>
         <div class="project-footer">
-            <span class="project-version">Version: ${project.version}</span>
-            <span class="project-tag">${project.tag}</span>
+            <span class="project-version">Version: ${plugin.version}</span>
+            <span class=" project-tag plugin-tag">Plugin</span>
         </div>
-    `
+    `;
 
-    div.innerHTML = innerHTML;
-
-    projectList.append(div)
+    projectList.append(div);
     div.style.cssText += `
         transform: translateY(-3%);
         opacity: 0;
         animation:project-ease-in 1000ms ease-out ${animDelay}ms;
         animation-fill-mode: forwards;
     `;
-
     animDelay += 120;
 }
