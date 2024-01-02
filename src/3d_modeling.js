9232e7b28416fd2feaec81c206c4e2956a745fcc
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-01-01 20:33:05 -0500

    Change style of portfolio a bit

--- src/3d_modeling.js
+++ src/3d_modeling.js
@@ -1,6 +1,6 @@
 const portfolioList = [
     {
-        "name": "ArtStation",
+        "name": "Portfolio",
         "description": "My portfolio containing my best 3D modeling work, with effective media and descriptions.",
         "link": "https://sirjain.artstation.com",
     }
@@ -10,6 +10,7 @@ const portfolioList = [
 const projectList = document.getElementById("portfolio-display");
 let animDelay = 0;
 
+// TODO: Cleanup b/c there is only one item
 for (const portfolio of portfolioList) {
     const div = document.createElement("portfolio");
     div.className = "portfolio";
@@ -17,19 +18,19 @@ for (const portfolio of portfolioList) {
 
     let innerHTML = `
         <div class="project-header">
-        <h3 class="project-title">${portfolio.name}</h2>
+        <h3 class="project-title" style="font-size: 26px;">${portfolio.name}</h2>
         <div class="spacer"></div>
     `
 
     innerHTML += `
         <a class="project-link" href="${portfolio.link}" target="_blank">
-            <span class="material-symbols-outlined" style="font-size: 20px;">open_in_new</span>
+            <span class="material-symbols-outlined" style="font-size: 22px;">open_in_new</span>
         </a>
         </div>
     `
 
     innerHTML += `
-        <p class="project-description">${portfolio.description}</p>
+        <p class="project-description" style="font-size: 18px;">${portfolio.description}</p>
         <div class="spacer" style="margin-top: 25px;"></div>
     `
 
