fe8cc72ba28da15ab2d05736ff7f26e001fee03a
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-01-01 20:38:12 -0500

    Portfolio section cleanup

--- src/3d_modeling.js
+++ src/3d_modeling.js
@@ -1,48 +1,30 @@
-const portfolioList = [
-    {
-        "name": "Portfolio",
-        "description": "My portfolio containing my best 3D modeling work, with effective media and descriptions.",
-        "link": "https://sirjain.artstation.com",
-    }
-]
-
 // Get the project-list element
 const projectList = document.getElementById("portfolio-display");
 let animDelay = 0;
 
-// TODO: Cleanup b/c there is only one item
-for (const portfolio of portfolioList) {
-    const div = document.createElement("portfolio");
-    div.className = "portfolio";
-    div.classList.add("portfolio-view");
+const div = document.createElement("portfolio");
+div.className = "portfolio";
+div.classList.add("portfolio-view");
 
-    let innerHTML = `
-        <div class="project-header">
-        <h3 class="project-title" style="font-size: 26px;">${portfolio.name}</h2>
-        <div class="spacer"></div>
-    `
+div.innerHTML = `
+    <div class="project-header">
+    <h3 class="project-title" style="font-size: 26px;">Portfolio</h2>
+    <div class="spacer"></div>
+    <a class="project-link" href="https://sirjain.artstation.com" target="_blank">
+        <span class="material-symbols-outlined" style="font-size: 22px;">open_in_new</span>
+    </a>
+    </div>
+    <p class="project-description" style="font-size: 18px;">My portfolio containing my best 3D modeling work, with effective media and descriptions.</p>
+    <div class="spacer" style="margin-top: 25px;"></div>
+`
 
-    innerHTML += `
-        <a class="project-link" href="${portfolio.link}" target="_blank">
-            <span class="material-symbols-outlined" style="font-size: 22px;">open_in_new</span>
-        </a>
-        </div>
-    `
+projectList.append(div);
 
-    innerHTML += `
-        <p class="project-description" style="font-size: 18px;">${portfolio.description}</p>
-        <div class="spacer" style="margin-top: 25px;"></div>
-    `
+div.style.cssText += `
+    transform: translateY(-3%);
+    opacity: 0;
+    animation:project-ease-in 1000ms ease-out ${animDelay}ms;
+    animation-fill-mode: forwards;
+`;
 
-    div.innerHTML = innerHTML;
-
-    projectList.append(div)
-    div.style.cssText += `
-        transform: translateY(-3%);
-        opacity: 0;
-        animation:project-ease-in 1000ms ease-out ${animDelay}ms;
-        animation-fill-mode: forwards;
-    `;
-
-    animDelay += 120;
-}
+animDelay += 120;
