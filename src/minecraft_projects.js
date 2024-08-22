6c0485b2633fe720e02a3c621b9ba49148a7b72e
SirJain <94301223+SirJain0@users.noreply.github.com>
2024-08-21 20:39:57 -0400

    Bump ED version

--- src/minecraft_projects.js
+++ src/minecraft_projects.js
@@ -2,7 +2,7 @@ const projects = [
     {
         "name": "Extensive Diamonds",
         "description": "Mod that adds three new diamond types to Minecraft!",
-        "version": "1.3.3",
+        "version": "1.3.5",
         "link": "https://www.curseforge.com/minecraft/mc-mods/extensive-diamonds/",
         "tag": "Mod"
     },
