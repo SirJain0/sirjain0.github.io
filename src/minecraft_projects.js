a104a67606fe6550a79eefa98286e8baf4f27f87
SirJain <94301223+SirJain0@users.noreply.github.com>
2026-01-20 12:19:21 -0500

    Bump cushy pillows version

--- src/minecraft_projects.js
+++ src/minecraft_projects.js
@@ -99,7 +99,7 @@ const projects = [
     {
         "name": "Cushy Pillows",
         "description": "Adds customizable, interactive pillows that you can sleep with, decorate, and throw at others!",
-        "version": "1.0.3",
+        "version": "1.0.34",
         "collaborators": "Daniel",
         "link": "https://www.curseforge.com/minecraft/mc-mods/cushy-pillows/",
         "tag": "Mod" 
